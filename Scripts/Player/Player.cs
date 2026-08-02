fcd40c6831b256451cf66e769fd60843e9c615ad
kanclusion <joniplay47@gmail.com>
2026-08-02 20:20:09 +0300

    feat: add movement

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -9,6 +9,7 @@ public partial class Player : CharacterBody3D
     [Export] public float JumpVelocity = 6f;
     [Export] public float MouseSensitivity = 0.005f;
     [Export] public float RotationSpeed = 10.0f;
+    [Export] public float BodyRotate = 5f;
 
     private Node3D _cameraPivot;
     private Camera3D _camera;
@@ -23,18 +24,35 @@ public partial class Player : CharacterBody3D
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
 
-        //Jump
+        //Falling
         if (!IsOnFloor())
         {
             velocity.Y -= _gravity * (float)delta;
         }
-        
+        //Falling
+
+        ////Jump
         if (Input.IsActionJustPressed("jump"))
         {
             velocity.Y += JumpVelocity;
         }
         //Jump
+
+        //Moving
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(velocity.Z, 0, Speed);
+        }
+        //Moving
 
         Velocity = velocity;
         MoveAndSlide();
